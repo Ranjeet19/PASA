b4baf96051c057d1377af05690da18c7e2495baa
Ranjeet19 <shrestharanjit2014@gmail.com>
2026-09-20 21:40:10 +0100

    Font

--- lib/view/drawer.dart
+++ lib/view/drawer.dart
@@ -11,7 +11,7 @@ class Draw extends StatelessWidget {
     const style = TextStyle(
         color: primaryColor,
         fontFamily: 'sans serif',
-        fontSize: 18,
+        fontSize: 16,
         fontWeight: FontWeight.w600);
     return Scaffold(
       body: ListView(
